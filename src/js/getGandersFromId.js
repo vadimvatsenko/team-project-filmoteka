ec6e4401a6da1df9a9061c1e65d6918dd030830b
vadimvatsenko <69381493+vadimvatsenko@users.noreply.github.com>
2022-10-27 16:37:19 +0300

    ganres.json
    Получение жанров

---
+++ src/js/getGandersFromId.js
@@ -0,0 +1,13 @@
+import {genres} from './genres.json'
+
+export function getfilmsGenres(ids) {
+  let filmsGenres = genres.reduce((acc, { id, name }) => {
+    if (ids.includes(id)) {
+      acc.push(name);
+    }
+    console.log(acc);
+    return acc;
+  }, []);
+    console.log(filmsGenres);
+   return filmsGenres;
+}
