6a7ba6146a7fcd6c084e5135f1c6af7e25064f23
Diakov Sasha <diakovsasha777@gmail.com>
2022-10-28 11:44:03 +0300

    add pagination

--- src/js/api.js
+++ src/js/api.js
@@ -1,6 +1,5 @@
 // import { createTopMovie } from './markup';
 
-
 // import { getRefs } from './refs';
 // const refs = getRefs();
 
@@ -13,7 +12,6 @@
 // const SEARCH_URL = `${BASE_URL}/search/movie?api_key=${API_KEY}&language=en-US&page=${page}&include_adult=false&query=`;
 // const TRENDING_URL = `${BASE_URL}/trending/movie/day?api_key=${API_KEY}&page=${page}`;
 // const GENRES_URL = `${BASE_URL}/genre/movie/list?api_key=${API_KEY}&language=en-US}`
-
 
 // async function getSearch(e) {
 //     e.preventDefault();
@@ -28,8 +26,7 @@
 //         console.log(error);
 //     }
 // }
-    
-  
+
 // getTrendingMovie();
 
 // async function getTrendingMovie() {
@@ -42,35 +39,84 @@
 //     } catch (error) {
 //         console.log(error);
 //     }
-    
+
 // }
 
-
 //! моє
-
 
 // import axios from 'axios';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-import { refs  } from './refs';
-import { getAPI, generateContent, pasteContent } from "./popularRender"
-import { getMovieNameAPI, generateContentNameAPI, pasteContentNameAPI} from "./renderByName"
-import { BASE_URL, KEY, IMG_URL, API_URL, POPULAR_URL, BASE_FIND_WORD_URL } from "./url"
+import { refs } from './refs';
+import { getAPI, generateContent, pasteContent } from './popularRender';
+import {
+  getMovieNameAPI,
+  generateContentNameAPI,
+  pasteContentNameAPI,
+} from './renderByName';
+import {
+  BASE_URL,
+  KEY,
+  IMG_URL,
+  API_URL,
+  POPULAR_URL,
+  BASE_FIND_WORD_URL,
+} from './url';
+// ==============================================================
 
+import Pagination from 'tui-pagination';
+import 'tui-pagination/dist/tui-pagination.css';
 
+// ================================================================
 //* рейтинг популярний фільмів при загрузці і перезавантаженні сайта
 getAPI(API_URL);
-
 
 //* запит і рендер фільмів за назвою
 function handleSubmit(event) {
   event.preventDefault();
-  
-  const movie = event.currentTarget.elements.search.value
-  .trim()
-  .toLowerCase();
-  console.log(movie);
-  // refs.list.innerHTML = "";
+
+  const movie = event.currentTarget.elements.search.value.trim().toLowerCase();
+
+  refs.list.innerHTML = '';
+  // =====================================================================
+  const options = {
+    totalItems: 2500,
+    itemsPerPage: 40,
+    visiblePages: 5,
+    page: 1,
+    centerAlign: true,
+    firstItemClassName: 'tui-first-child',
+    lastItemClassName: 'tui-last-child',
+    template: {
+      page: '<a href="#" class="tui-page-btn">{{page}}</a>',
+      currentPage:
+        '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
+      moveButton:
+        '<a href="#" class="tui-page-btn tui-{{type}}">' +
+        '<span class="tui-ico-{{type}}">{{type}}</span>' +
+        '</a>',
+      disabledMoveButton:
+        '<span class="tui-page-btn tui-is-disabled tui-{{type}}">' +
+        '<span class="tui-ico-{{type}}">{{type}}</span>' +
+        '</span>',
+      moreButton:
+        '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip">' +
+        '<span class="tui-ico-ellip">...</span>' +
+        '</a>',
+    },
+  };
+
+  const pagination = new Pagination('pagination', options);
+  pagination.on('afterMove', function (eventData) {
+    resetGallery();
+
+    getMovieNameAPI(movie, eventData.page);
+  });
+
+  function resetGallery() {
+    refs.list.innerHTML = '';
+  }
+  // =============================================================
 
   if (!movie) {
     Notify.failure(
@@ -78,8 +124,8 @@ function handleSubmit(event) {
     );
     return;
   }
-  refs.list.innerHTML = "";
-  getMovieNameAPI(movie)
+  refs.list.innerHTML = '';
+  getMovieNameAPI(movie);
 }
 
 refs.form.addEventListener('submit', handleSubmit);
--- src/js/renderByName.js
+++ src/js/renderByName.js
@@ -1,27 +1,36 @@
-import { BASE_URL, KEY, IMG_URL, API_URL , POPULAR_URL, BASE_FIND_WORD_URL } from "./url"
-import { refs  } from './refs';
-import { pasteContent } from "./createListItem"
+import {
+  BASE_URL,
+  KEY,
+  IMG_URL,
+  API_URL,
+  POPULAR_URL,
+  BASE_FIND_WORD_URL,
+} from './url';
+import { refs } from './refs';
+import { pasteContent } from './createListItem';
 
-export function getMovieNameAPI(movie) {
-fetch(`${BASE_FIND_WORD_URL}&query=${movie}`)
-.then(response => {
-  if (!response.ok) {
-    throw new Error(response.status);
-  }
-  return response.json();
-})
-.then(data => {
-  console.log(data);
+//
+// добавил page в параметр и в строку запроса!!!
+export function getMovieNameAPI(movie, page) {
+  fetch(`${BASE_FIND_WORD_URL}&page=${page}&query=${movie}`)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      return response.json();
+    })
+    .then(data => {
+      console.log(data);
 
-  if(data.results.length !== 0){
-    pasteContent(data.results);
-  } else {
-      refs.list.innerHTML = `<h1 class="list__nofind">No movies found on request :(</h1>`
-  }
-})
-.catch(error => {
-  console.log('error', error);
-});
+      if (data.results.length !== 0) {
+        pasteContent(data.results);
+      } else {
+        refs.list.innerHTML = `<h1 class="list__nofind">No movies found on request :(</h1>`;
+      }
+    })
+    .catch(error => {
+      console.log('error', error);
+    });
 }
 
 //! Фетч фільмів по назві фільма
