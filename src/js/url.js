a9a0cc2372cf6ce1a9c899bbb0e4023b11ad45bd
Vitalii Blyskun <97634483+VitaliiBlyskun@users.noreply.github.com>
2022-10-29 11:55:58 +0300

    add fixes api card
    add fixes strings inside the card

--- src/js/url.js
+++ src/js/url.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 export const BASE_URL = 'https://api.themoviedb.org/3/'; //базове юрл
 export const KEY = '00bb2c85647763d13c7f7e27b824373c'; //ключ
 export const IMG_URL = 'https://image.tmdb.org/t/p/w500'; // посилання на картинку
-export const API_URL = `${BASE_URL}trending/all/day?api_key=${KEY}`; // посилання на початковий рендер популярних фільмів
+export const API_URL = `${BASE_URL}trending/movie/day?api_key=${KEY}`; // посилання на початковий рендер популярних фільмів
 export const BASE_FIND_WORD_URL = `https://api.themoviedb.org/3/search/movie?api_key=00bb2c85647763d13c7f7e27b824373c`; // посилання на пошук фільмів за назвою
 // export const POPULAR_URL = `${BASE_URL}discover/movie/?sort_by=popularity.desc&api_key=${KEY}`;
 export const POPULAR_URL = `${BASE_URL}/movie/popular?api_key=${KEY}&language=en-US`;
