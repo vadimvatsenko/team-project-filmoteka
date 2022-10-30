e9ad8d2a6782495b534e8604c9fcb855eb69407c
Kristina Rachkovich <ra4kovi4@ukr.net>
2022-10-30 18:44:13 +0200

    подключила спинер к фильтру

--- src/js/filter.js
+++ src/js/filter.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import { refs } from './refs';
 import { getAPI } from './popularRender';
 import { generateContent } from './createListItem';
-import { spinerStart, spinerStop } from './spiner';
+import { Loading, Notify } from 'notiflix';
 import Pagination from 'tui-pagination';
 
 export const filterItem = {
@@ -24,7 +24,12 @@ let formSearch = {
 };
 
 //запрос на бэкенд по жанру и/или году
-export async function getSearchForm(genre = '', year = '', page = '', query = '') {
+export async function getSearchForm(
+  genre = '',
+  year = '',
+  page = '',
+  query = ''
+) {
   formSearch.year =
     year !== '' && year !== 'start' ? `&primary_release_year=${year}` : '';
   formSearch.genre =
@@ -45,10 +50,16 @@ export async function getSearchForm(genre = '', year = '', page = '', query = ''
     formSearch.discover = 'discover';
   }
   const fetchCard = await axios.get(
-    `${BASE_URL}${formSearch.discover}/movie?sort_by=popularity.desc&with_genres=${genre === 'start' ? " ": genre}&primary_release_year=${year === 'start' ? " ": year}&include_adult=false&api_key=${KEY}&page=${page}`
+    `${BASE_URL}${
+      formSearch.discover
+    }/movie?sort_by=popularity.desc&with_genres=${
+      genre === 'start' ? ' ' : genre
+    }&primary_release_year=${
+      year === 'start' ? ' ' : year
+    }&include_adult=false&api_key=${KEY}&page=${page}`
   );
-    localStorage.setItem('totalItems' ,  fetchCard.data.total_results)
-    localStorage.setItem('itemsPerPage', fetchCard.data.results.length)
+  localStorage.setItem('totalItems', fetchCard.data.total_results);
+  localStorage.setItem('itemsPerPage', fetchCard.data.results.length);
   return fetchCard.data;
 }
 
@@ -60,18 +71,33 @@ filterItem.resetButton.addEventListener('click', onResetSearch);
 //Поиск рендер фильма по жанру
 function eventGenre(evt) {
   evt.preventDefault();
-  spinerStart;
+
   if (evt) {
     formSearch.genre = evt.target.value;
     formSearch.page = 1;
     getSearchForm(formSearch.genre, formSearch.year, formSearch.page)
       .then(data => {
-        console.log(data);
-        renderFiltrMarkup(data.results);
+        Loading.custom('Loading...', {
+          customSvgCode:
+            '<svg width="16" height="12" viewBox="0 0 16 12" fill="none" xmlns="http://www.w3.org/2000/svg"><linearGradient id="myG"  fy="0" gradientTransform="rotate(60 .5 .5)"><stop offset="0" stop-color="#f15361"></stop><stop offset=".25" stop-color="#ffffff"><animate attributeName="offset" dur="2s" values="0;1;0"repeatCount="indefinite" /></stop><stop offset="1" stop-color="#f15361"/></linearGradient><path d="M0 0V12H16V0H0ZM3 11H1V9H3V11ZM3 7H1V5H3V7ZM3 3H1V1H3V3ZM12 11H4V1H12V11ZM15 11H13V9H15V11ZM15 7H13V5H15V7ZM15 3H13V1H15V3ZM6 3V9L10 6L6 3Z" fill="url(#myG)"/></svg>',
+          backgroundColor: 'rgba(0, 0, 0, 0.8)',
+        });
+
+        if (data.results.length !== 0) {
+          renderFiltrMarkup(data.results);
+        } else {
+          Loading.custom('Loading...', {
+            customSvgCode:
+              '<svg width="16" height="12" viewBox="0 0 16 12" fill="none" xmlns="http://www.w3.org/2000/svg"><linearGradient id="myG"  fy="0" gradientTransform="rotate(60 .5 .5)"><stop offset="0" stop-color="#f15361"></stop><stop offset=".25" stop-color="#ffffff"><animate attributeName="offset" dur="2s" values="0;1;0"repeatCount="indefinite" /></stop><stop offset="1" stop-color="#f15361"/></linearGradient><path d="M0 0V12H16V0H0ZM3 11H1V9H3V11ZM3 7H1V5H3V7ZM3 3H1V1H3V3ZM12 11H4V1H12V11ZM15 11H13V9H15V11ZM15 7H13V5H15V7ZM15 3H13V1H15V3ZM6 3V9L10 6L6 3Z" fill="url(#myG)"/></svg>',
+            backgroundColor: 'rgba(0, 0, 0, 0.8)',
+          });
+          Notify.failure(`Nothing was found for your request`);
+          refs.list.innerHTML = ` <img src="${'https://cdn.dribbble.com/users/4266416/screenshots/8269080/media/69de53b0834d3b0c493f21d4ce773dfd.png'}"
+          alt="no movies found" class="no__found">`;
+        }
       })
       .catch(error => console.log(error))
-      .finally(() => spinerStop);
-    
+      .finally(() => Loading.remove(2000));
   }
 }
 
@@ -81,17 +107,32 @@ function eventYear(evt) {
   // localStorage.setItem("genre", formSearch.genre? formSearch.genre:" ");
   //     localStorage.setItem("year", formSearch.year? formSearch.year: " ");
   if (evt) {
-    spinerStart;
     formSearch.year = Number(evt.target.value);
     formSearch.page = 1;
 
     getSearchForm(formSearch.genre, formSearch.year, formSearch.page)
       .then(data => {
-        refs.list.innerHTML = '';
-        renderFiltrMarkup(data.results);
+        Loading.custom('Loading...', {
+          customSvgCode:
+            '<svg width="16" height="12" viewBox="0 0 16 12" fill="none" xmlns="http://www.w3.org/2000/svg"><linearGradient id="myG"  fy="0" gradientTransform="rotate(60 .5 .5)"><stop offset="0" stop-color="#f15361"></stop><stop offset=".25" stop-color="#ffffff"><animate attributeName="offset" dur="2s" values="0;1;0"repeatCount="indefinite" /></stop><stop offset="1" stop-color="#f15361"/></linearGradient><path d="M0 0V12H16V0H0ZM3 11H1V9H3V11ZM3 7H1V5H3V7ZM3 3H1V1H3V3ZM12 11H4V1H12V11ZM15 11H13V9H15V11ZM15 7H13V5H15V7ZM15 3H13V1H15V3ZM6 3V9L10 6L6 3Z" fill="url(#myG)"/></svg>',
+          backgroundColor: 'rgba(0, 0, 0, 0.8)',
+        });
+
+        if (data.results.length !== 0) {
+          renderFiltrMarkup(data.results);
+        } else {
+          Loading.custom('Loading...', {
+            customSvgCode:
+              '<svg width="16" height="12" viewBox="0 0 16 12" fill="none" xmlns="http://www.w3.org/2000/svg"><linearGradient id="myG"  fy="0" gradientTransform="rotate(60 .5 .5)"><stop offset="0" stop-color="#f15361"></stop><stop offset=".25" stop-color="#ffffff"><animate attributeName="offset" dur="2s" values="0;1;0"repeatCount="indefinite" /></stop><stop offset="1" stop-color="#f15361"/></linearGradient><path d="M0 0V12H16V0H0ZM3 11H1V9H3V11ZM3 7H1V5H3V7ZM3 3H1V1H3V3ZM12 11H4V1H12V11ZM15 11H13V9H15V11ZM15 7H13V5H15V7ZM15 3H13V1H15V3ZM6 3V9L10 6L6 3Z" fill="url(#myG)"/></svg>',
+            backgroundColor: 'rgba(0, 0, 0, 0.8)',
+          });
+          Notify.failure(`Nothing was found for your request`);
+          refs.list.innerHTML = ` <img src="${'https://cdn.dribbble.com/users/4266416/screenshots/8269080/media/69de53b0834d3b0c493f21d4ce773dfd.png'}"
+          alt="no movies found" class="no__found">`;
+        }
       })
       .catch(error => console.log(error))
-      .finally(() => spinerStop);
+      .finally(() => Loading.remove(2000));
   }
 }
 
