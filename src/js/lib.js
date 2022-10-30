28ceae6c2ccdf926e671e45d6fe7a138897e3292
artipavl <40296816+artipavl@users.noreply.github.com>
2022-10-30 17:27:45 +0200

    Пагінація для бібліотеки

--- src/js/lib.js
+++ src/js/lib.js
@@ -9,9 +9,12 @@ btnWatched.addEventListener('click', rWatched);
 
 btnQueue.addEventListener('click', rQueue);
 
+const localWatched = JSON.parse(localStorage.getItem('watched')).watched;
+const localQueue = JSON.parse(localStorage.getItem('watched')).queue;
+
 export function rWatched() {
+  getWatched("watched", 1)
   const localWatched = JSON.parse(localStorage.getItem('watched')).watched;
-  listWatched.innerHTML = localWatched.join('');
   if (localWatched.length === 0) {
     listWatched.innerHTML = '';
     libSection.classList.add('non-display');
@@ -26,8 +29,8 @@ export function rWatched() {
 }
 
 export function rQueue() {
-  const localQueue = JSON.parse(localStorage.getItem('watched')).queue;
-  listWatched.innerHTML = localQueue.join('');
+  getWatched("queue", 1)
+const localQueue = JSON.parse(localStorage.getItem('watched')).queue;
   if (localQueue.length === 0) {
     listWatched.innerHTML = '';
     libSection.classList.add('non-display');
@@ -36,13 +39,27 @@ export function rQueue() {
     libSection.classList.remove('non-display');
     bacgroundLib.classList.add('non-display');
   }
+
   btnQueue.classList.add('button--lib-active');
   btnWatched.classList.remove('button--lib-active');
 }
 
-const localWatched = JSON.parse(localStorage.getItem('watched')).watched;
+
 if (localWatched.length === 0) {
   rQueue();
 } else {
   rWatched();
+}
+
+export function getWatched(name, page) {
+  const localWatched = JSON.parse(localStorage.getItem('watched'))[name];
+  console.log(localWatched);
+  let begin = 20 * page -20;
+  const end = 20 * page;
+  if (page === 1) {
+    begin = 0;
+  }
+  
+  const slice = localWatched.slice(begin, end).join();
+  listWatched.innerHTML = slice;
 }
