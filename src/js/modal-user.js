917b2454a5c45d10a96d77a3107cda8d4af29a80
arturtretyak <arturtretyak1990@gmail.com>
2022-11-02 01:28:36 +0200

    Changes for merge

---
+++ src/js/modal-user.js
@@ -0,0 +1,30 @@
+import { refs } from './refs';
+import ModalClassic from './modalClassic';
+import { createUser } from './firebase';
+
+const Modal = new ModalClassic('.wrapper-modal-user', '.modal-user__btn-close');
+
+refs.loginBtn?.addEventListener('click', evt => {
+  Modal.openModal();
+});
+
+const formRegister = document.querySelector('.modal-register');
+
+formRegister?.addEventListener('submit', registrationHandler);
+
+function registrationHandler(evt) {
+  evt.preventDefault();
+  const user = {
+    email: evt.currentTarget['email-reg'].value,
+    password: evt.currentTarget['password-reg'].value,
+  };
+  //   console.log(user);
+  const result = createUser(user.email, user.password)
+    .then(data => {
+      console.log(data.uid);
+      localStorage.setItem('id-user', data.uid);
+    })
+    .catch(e => {
+      console.log(e);
+    });
+}
